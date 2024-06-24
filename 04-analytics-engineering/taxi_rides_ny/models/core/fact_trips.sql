f8d7b45f62e11dd1329f4fcfa9869bdd0e78cae1
Thanh Nguyen <thanh.ntt0504@gmail.com>
2024-06-24 03:19:10 +0000

    update-24-06

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
@@ -7,18 +7,18 @@
 with green_tripdata as (
     select *, 
         'Green' as service_type
-    from {{ ref('stg_staging__green_tripdata') }}
+    from {{ ref('stg_green_tripdata') }}
 ), 
 yellow_tripdata as (
     select *, 
         'Yellow' as service_type
-    from {{ ref('stg_staging__yellow_tripdata') }}
+    from {{ ref('stg_yellow_tripdata') }}
 ), 
 trips_unioned as (
     select * from green_tripdata
     union all 
     select * from yellow_tripdata
-), 
+),  
 dim_zones as (
     select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
